89fe9872174b8c4574d674e2fccfc85515c872f4
Hanna <hanna.botar@teamnet.ro>
2018-05-02 19:43:21 +0300

    artists - progress

---
+++ app/src/main/java/com/example/hannabotar/musicalstructure/adapter/ArtistAdapter.java
@@ -0,0 +1,4 @@
+package com.example.hannabotar.musicalstructure.adapter;
+
+public class ArtistAdapter {
+}
---
+++ app/src/main/java/com/example/hannabotar/musicalstructure/model/Artist.java
@@ -0,0 +1,4 @@
+package com.example.hannabotar.musicalstructure.model;
+
+public class Artist {
+}
